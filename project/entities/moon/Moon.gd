31ff51efd98698606b5eb3ca0591f6e358e2526b
Gabriel keith <gabe.keith@yahoo.com>
2020-11-20 12:01:13 -0500

    Music?

--- project/entities/moon/Moon.gd
+++ project/entities/moon/Moon.gd
@@ -1,4 +1,4 @@
 extends "res://entities/Planetoid.gd"
 
-func get_input():
-	return position.direction_to($"../Player".position)
+#func get_input():
+#	return position.direction_to($"../Player".position)
--- project/entities/player/Player.gd
+++ project/entities/player/Player.gd
@@ -1,5 +1,10 @@
 extends "res://entities/Planetoid.gd"
 
+const Moon = preload("res://entities/moon/Moon.gd")
+
+var _moon_set := {}
+var _moons := []
+
 func get_input() -> Vector2:
 	var input := Vector2()
 	if Input.is_action_pressed('right'):
@@ -11,3 +16,7 @@ func get_input() -> Vector2:
 	if Input.is_action_pressed('up'):
 		input.y -= 1
 	return input
+
+
+#func _on_moon_entered(moon: Moon):
+#	_moons.append(mode)
