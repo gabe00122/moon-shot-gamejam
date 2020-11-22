c058439f89efdfc73b832c1828cfb603bfcb4b78
gabe00122 <gabe.keith@yahoo.com>
2020-11-22 13:14:50 -0500

    Progress on orbits

--- project/entities/player/Player.gd
+++ project/entities/player/Player.gd
@@ -2,7 +2,6 @@ extends "res://entities/Planetoid.gd"
 
 const Moon = preload("res://entities/moon/Moon.gd")
 
-var _moon_set := {}
 var _moons := []
 
 func get_input() -> Vector2:
@@ -15,8 +14,25 @@ func get_input() -> Vector2:
 		input.y += 1
 	if Input.is_action_pressed('up'):
 		input.y -= 1
+	
 	return input
 
+func _physics_process(delta):
+	if Input.is_action_just_pressed("fire_up"):
+		_fire_moon(Vector2(0, -1))
+	elif Input.is_action_just_pressed("fire_down"):
+		_fire_moon(Vector2(0, 1))
+	elif Input.is_action_just_pressed("fire_left"):
+		_fire_moon(Vector2(-1, 0))
+	elif Input.is_action_just_pressed("fire_right"):
+		_fire_moon(Vector2(1, 0))
 
-#func _on_moon_entered(moon: Moon):
-#	_moons.append(mode)
+func _fire_moon(direction: Vector2):
+	if !_moons.empty():
+		var moon: Moon = _moons.pop_front()
+		moon.fire(direction)
+
+func _on_moon_entered(moon: Moon):
+	if moon.orbit_target == null:
+		moon.orbit_target = self
+		_moons.append(moon)
